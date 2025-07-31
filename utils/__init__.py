e9be58dd7b300e446764829025274ae217fa2f9e
Emmanuel <ecobian12310@gmail.com>
2025-07-31 11:15:03 -0700

    removed unused init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from .video_utils import read_video, save_video, read_highlights, stream_video_frames, get_video_info, StreamingVideoWriter
 from .stub_utils import save_stub, read_stub
 from .bbox_utils import get_bbox_center, get_bbox_width, get_bbox_height, measure_distance
-from .global_id_manager import GlobalIDManager
 from .score_utils import score, get_device, detect_down, detect_up, find_closest_player
